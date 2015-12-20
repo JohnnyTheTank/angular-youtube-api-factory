1803dcac53f7a6cc8e95a04de2e2e4d30f4cb296
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-20 16:33:27 +0100

    impr: created demo (close #6)

---
+++ demo/js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module("app", ['jtt_youtube']);
+app.controller('controller', ['$scope', 'youtubeFactory', function($scope, youtubeFactory) {
+
+    var _apiKey = "<YOUR_YOUTUBE_API_KEY>";
+
+    youtubeFactory.getVideosFromChannelById({
+        channelId: "UCVkXCOYluJvD6OPjX9HXj-A",
+        maxResults: "50", // (optional) default: 5
+        key: _apiKey,
+    }).success(function (_data) {
+        console.info("videos from channel by id", _data);
+    });
+
+    youtubeFactory.getVideosFromSearchByString({
+        q: "fcbayern",
+        maxResults: "50", // (optional) default: 5
+        key: _apiKey,
+    }).success(function (_data) {
+        console.info("videos from search by string", _data);
+    });
+
+    youtubeFactory.getVideosFromPlaylistById({
+        playlistId: "PLNLa2lbKPczGCueOYxjrwYDuNTBtQveK0",
+        maxResults: "50", // (optional) default: 5
+        key: _apiKey,
+    }).success(function (_data) {
+        console.info("videos from playlist by id", _data);
+    });
+
+    youtubeFactory.getChannelById({
+        channelId: "UCVkXCOYluJvD6OPjX9HXj-A",
+        key: _apiKey,
+    }).success(function (_data) {
+        console.info("channel by id", _data);
+    });
+
+
+}]);
